2fc8410a07a3ebf9338b2b5dc6dd193933cfdaec
Shivam Chaurasia <avthanos004@gmail.com>
2026-08-06 01:15:38 +0530

    Implement JWT authentication and secure user registration

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -4,7 +4,15 @@ from sqlalchemy.orm import Session
 
 from app.database.database import get_db
 from app.schemas.user import UserCreate, UserResponse
-from app.services.user_service import create_user
+# from app.services.user_service import create_user
+from app.services.user_service import (
+    create_user,
+    get_users,
+    get_user,
+    update_user,
+    delete_user
+)
+
 
 # router = APIRouter()
 router = APIRouter(prefix="/api/v1", tags=["CareerPilot API"])
@@ -26,3 +34,37 @@ def create_new_user(
     db: Session = Depends(get_db)
 ):
     return create_user(db, user)
+
+
+
+@router.get("/users", response_model=list[UserResponse])
+def get_all_users(
+    db: Session = Depends(get_db)
+):
+    return get_users(db)
+
+
+@router.get("/users/{user_id}", response_model=UserResponse)
+def get_single_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    return get_user(db, user_id)
+
+
+@router.put("/users/{user_id}", response_model=UserResponse)
+def update_existing_user(
+    user_id: int,
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    return update_user(db, user_id, user)
+
+
+@router.delete("/users/{user_id}")
+def delete_existing_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    delete_user(db, user_id)
+    return {"message": "User deleted successfully"}
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,16 +1,60 @@
+from app.utils.security import hash_password
 from sqlalchemy.orm import Session
 
 from app.models.user import User
 from app.schemas.user import UserCreate
+# def create_user(db: Session, user: UserCreate):
+#     db_user = User(
+#         full_name=user.full_name,
+#         email=user.email,
+#         password=user.password
+#     )
+
+#     db.add(db_user)
+#     db.commit()
+#     db.refresh(db_user)
+
+
+    # return db_user
 def create_user(db: Session, user: UserCreate):
     db_user = User(
         full_name=user.full_name,
         email=user.email,
-        password=user.password
+        password=hash_password(user.password)
     )
 
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
+
+    return db_user
+def get_users(db: Session):
+    return db.query(User).all()
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+
+def update_user(db: Session, user_id: int, user: UserCreate):
+    db_user = db.query(User).filter(User.id == user_id).first()
+
+    if db_user:
+        db_user.full_name = user.full_name
+        db_user.email = user.email
+        db_user.password = user.password
+
+        db.commit()
+        db.refresh(db_user)
+
+    return db_user
+
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(User).filter(User.id == user_id).first()
+
+    if db_user:
+        db.delete(db_user)
+        db.commit()
 
     return db_user
